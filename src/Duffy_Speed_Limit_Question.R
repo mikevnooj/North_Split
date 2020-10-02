74e70b3ffae85a6ee79467f9169926a05563dd83
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-10-02 16:46:08 -0400

    start map analysis, change names in viz

--- src/Duffy_Speed_Limit_Question.R
+++ src/Duffy_Speed_Limit_Question.R
@@ -15,6 +15,8 @@ library(sf)
 library(leaflet)
 library(leafem)
 library(RANN)
+library(rgeos)
+library(ggplot2)
 
 # import DW Fact_Seg_Adh --------------------------------------------------
 
@@ -52,8 +54,16 @@ DimRoute_90 <- tbl(con_DW, "DimRoute") %>%
   filter(RouteReportLabel == "90") %>%
   collect() %>%
   data.table(key = "RouteKey")
-DimRoute_90
 
+DimPattern <- tbl(con_DW,"DimPattern") %>% 
+  collect() %>%
+  data.table(key = "PatternKey")
+
+DimTime <- tbl(con_DW,"DimTime") %>%
+  collect() %>%
+  data.table(key = "TimeKey")
+
+DimTime <- DimTime[,.(TimeKey,Time24Desc)]
 
 #get fact seg adh
 FSA_raw <- tbl(con_DW, "FactSegmentAdherence") %>% 
@@ -64,100 +74,211 @@ FSA_raw <- tbl(con_DW, "FactSegmentAdherence") %>%
   data.table()
 
 
+# clean FSA ---------------------------------------------------------------
 
-
-
-# read in current GTFS ----------------------------------------------------
-
-current_gtfs <- read_gtfs("data//GTFS//2006_2//2006_2_GTFS.zip")
-
-# convert to sf
-current_gtfs_sf <- gtfs_as_sf(current_gtfs) %>% set_hms_times %>% set_date_service_table() %>% set_servicepattern()
-
-#remove trips with no shapes
-current_gtfs_sf$trips %<>%
-  filter(!is.na(shape_id))
-
-current_trips_and_shapes_sf <- current_gtfs_sf$shapes %>% 
-  right_join(current_gtfs_sf$trips)
-
-# join routes as well
-current_route_trips_and_shapes_sf <- current_trips_and_shapes_sf %>%
-  right_join(current_gtfs_sf$routes)
-
-#stops df for ease of use
-current_stops_sf <- Filter(function(x)!all(is.na(x)),current_gtfs_sf$stops) 
-
-
-# G is northbound
-# get nb and sb ends
-endpoint_stop_names <- c("State.*NB","Broa.*SB")
-
-segment_end_points_sf <- current_stops_sf %>%
-  filter(grepl(paste(endpoint_stop_names,collapse = "|"),stop_name))
-
-#get 90 direction and shape
-route_shape_sf <- current_route_trips_and_shapes_sf %>%
-  filter(route_short_name == 90) %>% #this gets the right set of shapes
-  filter(st_length(geometry) == max(st_length(geometry))) %>% 
-  slice(1)
-
-#get route points
-route_shape_point_sf <- current_gtfs$shapes %>% 
-  st_as_sf(coords = c("shape_pt_lon","shape_pt_lat"),
-           crs = 4326) %>% 
-  filter(shape_id == route_shape_sf$shape_id)
-
-#match points to endpoints
-#this nn2 works identically
-route_point_indices <- route_shape_point_sf %>%
-  st_coordinates %>%
-  nn2(query = segment_end_points_sf %>% st_coordinates()
-      ,k = 2) %>%
-  .$nn.idx %>%
-  apply(1, min)
-
-#input for next
-n <- nrow(segment_end_points_sf)
-
-#split into segment
-segment_shape <- route_shape_point_sf[route_point_indices[1]:route_point_indices[2],] %>%
-  group_by(shape_id) %>%
-  arrange(shape_pt_sequence) %>%
-  summarize(do_union = F) %>%
-  st_cast("LINESTRING")
-
-#turn current stops into dt
-current_stops_geo_dt <- data.table(current_stops_sf,key = "stop_id")
-
-segment_shape %>% 
-  leaflet() %>%
-  addFeatures() %>%
-  addTiles()
-  
-
-FSA_joined <- DimStop[current_stops_geo_dt, on = c(StopID = "stop_id")
+FSA_Speed <- FSA_raw[
+  #join by ref, saves mem
+  DimPattern
+  ,on = "PatternKey"
+  ,names(DimPattern) := mget(paste0("i.",names(DimPattern)))
 ][
-  ,.(StopKey,StopDesc,StopID,geometry)
+  #join time
+  DimTime
+  ,on = c(ArriveTimeKey = "TimeKey")
+  ,names(DimTime) := mget(paste0("i.",names(DimTime)))
 ][
-  FSA_raw
-  ,on = c(StopKey = "DepartStopKey")
+  #set peak, non peak
+  ,Peak := fcase(
+    Time24Desc >= "06:00" & Time24Desc < "09:00","AM"
+    ,Time24Desc >= "15:00" & Time24Desc < "18:00", "PM"
+    ,default = "Off-Peak" 
+  )
 ][
-  ,`:=`(DepartStopKey = StopKey
-        ,DepartStop = StopDesc
-        ,DepartStopID = StopID
-        ,DepartStopGeometry = geometry)
+  #filter out deadhead
+  DeadheadInd != 1
+][
+  #get mph
+  ,`:=` (
+    MPH_Schedule = (DistanceFeetSchedule/5280)/(DriveTimeScheduleSecs/3600)
+    ,MPH_Actual = (DistanceFeetSchedule/5280)/(DriveTimeActualSecs/3600)
+  )
+][
+  #get variance
+  ,`:=` (
+    MPH_Variance = MPH_Schedule - MPH_Actual
+    ,MPH_Variance_pct = MPH_Actual/MPH_Schedule
+  )
+][
+  #now clean a little bit
+  MPH_Variance_pct >= 0 &
+    is.finite(MPH_Variance_pct)
+][
+  MPH_Actual < quantile(MPH_Actual,.99)
 ]
 
-x<-tbl(
- con_DW
- ,sql(
-   "select RouteSegmentKey
-   ,StartingStopKey
-   ,EndingStopKey
-   ,Geom
-   from DimRouteSegment
-   where RouteSegmentKey = 11010"
- )
-) %>%
-  collect()
+
+# # detour ------------------------------------------------------------------
+# 
+FSA_Speed[MPH_Actual <= quantile(MPH_Actual,.99)]
+
+FSA_Speed[,.N,keyby = .(gr = cut(MPH_Actual, breaks = seq(0,max(MPH_Actual),by = 10)))]
+FSA_Speed[,summary(MPH_Actual)]
+FSA_Speed[MPH_Actual < quantile(MPH_Actual,.99),summary(MPH_Actual)]
+
+#get median speeds
+Stat_Segment_MPH <- FSA_Speed[
+  #do by direction, peak period
+  ,.(Median_MPH_Actual = median(MPH_Actual)
+     ,Median_MPH_Sched = median(MPH_Schedule)
+     ,Max_MPH_Actual = max(MPH_Actual)
+     ,Min_MPH_Actual = min(MPH_Actual)
+     ,pct_95 = quantile(MPH_Actual,.95))
+  ,.(RouteSegmentKey, PatternDirectionDesc,Peak)
+]
+
+
+
+# Now for the Mapping -----------------------------------------------------
+# now get route segment
+# DimRouteSegment <- tbl(
+#   con_DW
+#   ,sql(
+#     "Select RouteSegmentKey
+#     ,Geom.STAsText() as ShapeWKT
+#     from dbo.DimRouteSegment"
+#   )
+# ) %>%
+#   collect() %>%
+#   data.table(key = "RouteSegmentKey")
+
+DimRouteSegment <- st_read(con_DW,geometry_column = "geom",
+           query = "
+           Select RouteSegmentKey
+           ,Geom.STAsBinary() as geom
+           from dbo.DimRouteSegment
+           where Geom IS NOT NULL
+           ",EWKB = TRUE,crs = 4326)
+
+merge(DimRouteSegment,Stat_Segment_MPH)
+
+Stat_Segment_MPH_joined_sf <- merge(DimRouteSegment,Stat_Segment_MPH) %>%
+  filter(RouteSegmentKey != 11311)
+
+pal <- colorNumeric(
+  palette = "YlOrRd"
+  ,domain = Stat_Segment_MPH_joined_sf$Median_MPH_Actual
+)
+
+Stat_Segment_MPH_joined_sf %>%
+  st_crop(xmin = -89.16179, xmax = -86.14440,
+          ymin = 39.76712, ymax = 39.88113) %>%
+  leaflet() %>%
+  addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  addFeatures(
+    color = ~pal(Median_MPH_Actual)
+    ,label = ~paste(Median_MPH_Actual,"/br",RouteSegmentKey
+  ) %>%
+  addMouseCoordinates()
+
+  
+
+ 
+# doesn't work for some reason will investigate later
+# Stat_Segment_MPH_joined <- Stat_Segment_MPH[
+#   #join route segment with geos
+#   DimRouteSegment
+#   ,on = "RouteSegmentKey"
+#   ,names(DimRouteSegment) := mget(paste0("i.",names(DimRouteSegment)))
+# ][
+#   #clean it up
+#   RouteSegmentKey != 11311
+# ]
+
+
+
+
+# # read in current GTFS ----------------------------------------------------
+# 
+# current_gtfs <- read_gtfs("data//GTFS//2006_2//2006_2_GTFS.zip")
+# 
+# # convert to sf
+# current_gtfs_sf <- gtfs_as_sf(current_gtfs) %>% set_hms_times %>% set_date_service_table() %>% set_servicepattern()
+# 
+# #remove trips with no shapes
+# current_gtfs_sf$trips %<>%
+#   filter(!is.na(shape_id))
+# 
+# current_trips_and_shapes_sf <- current_gtfs_sf$shapes %>% 
+#   right_join(current_gtfs_sf$trips)
+# 
+# # join routes as well
+# current_route_trips_and_shapes_sf <- current_trips_and_shapes_sf %>%
+#   right_join(current_gtfs_sf$routes)
+# 
+# #stops df for ease of use
+# current_stops_sf <- Filter(function(x)!all(is.na(x)),current_gtfs_sf$stops) 
+# 
+# 
+# # G is northbound
+# # get nb and sb ends
+# endpoint_stop_names <- c("State.*NB","Broa.*SB")
+# 
+# segment_end_points_sf <- current_stops_sf %>%
+#   filter(grepl(paste(endpoint_stop_names,collapse = "|"),stop_name))
+# 
+# #get 90 direction and shape
+# route_shape_sf <- current_route_trips_and_shapes_sf %>%
+#   filter(route_short_name == 90) %>% #this gets the right set of shapes
+#   filter(st_length(geometry) == max(st_length(geometry))) %>% 
+#   slice(1)
+# 
+# #get route points
+# route_shape_point_sf <- current_gtfs$shapes %>% 
+#   st_as_sf(coords = c("shape_pt_lon","shape_pt_lat"),
+#            crs = 4326) %>% 
+#   filter(shape_id == route_shape_sf$shape_id)
+# 
+# #match points to endpoints
+# #this nn2 works identically
+# route_point_indices <- route_shape_point_sf %>%
+#   st_coordinates %>%
+#   nn2(query = segment_end_points_sf %>% st_coordinates()
+#       ,k = 2) %>%
+#   .$nn.idx %>%
+#   apply(1, min)
+# 
+# #input for next
+# n <- nrow(segment_end_points_sf)
+# 
+# #split into segment
+# segment_shape <- route_shape_point_sf[route_point_indices[1]:route_point_indices[2],] %>%
+#   group_by(shape_id) %>%
+#   arrange(shape_pt_sequence) %>%
+#   summarize(do_union = F) %>%
+#   st_cast("LINESTRING")
+# 
+# #turn current stops into dt
+# current_stops_geo_dt <- data.table(current_stops_sf,key = "stop_id")
+# 
+# segment_shape %>% 
+#   leaflet() %>%
+#   addFeatures() %>%
+#   addTiles()
+#   
+# 
+# # FSA_joined <- DimStop[current_stops_geo_dt, on = c(StopID = "stop_id")
+# # ][
+# #   ,.(StopKey,StopDesc,StopID,geometry)
+# # ][
+# #   FSA_raw
+# #   ,on = c(StopKey = "DepartStopKey")
+# # ][
+# #   ,`:=`(DepartStopKey = StopKey
+# #         ,DepartStop = StopDesc
+# #         ,DepartStopID = StopID
+# #         ,DepartStopGeometry = geometry)
+# # ]
+# 
+# 
+# 
+# 
--- src/viz_analysis.R
+++ src/viz_analysis.R
@@ -21,15 +21,17 @@ holidays_saturday_service <- c("USMLKingsBirthday")
 holidays_sunday <- holiday(2000:2020, holidays_sunday_service)
 holidays_saturday <- holiday(2000:2020, holidays_saturday_service)
 
+
+
 #add labels
-VMH_speed_DT[,.id := ifelse(.id == "reversible (college)","single bi-directional",ifelse(.id == "dedicated","separated uni-directional","none"))]
+VMH_speed_DT[,lane_type := ifelse(.id == "reversible (college)","college (bi-directional median)",ifelse(.id == "dedicated","downtown (curbside exclusive)","virginia/shelby (none)"))]
 
 VMH_speed_DT_clean <- VMH_speed_DT[speed < Inf & 
                                      Vehicle_ID %in% c(1899,1970:1999)
                                    ][, `:=`(tpercrk = rank(time)/length(time)
                                             ,spercrk = rank(speed)/length(speed)
                                             ,travperc = miles_traveled/max(miles_traveled))
-                                     ,.(.id,direction)][ 
+                                     ,.(lane_type,direction)][ 
                                        #tpercrk > .05 
                                          #tpercrk < .95 &
                                          #spercrk < .95  
@@ -48,6 +50,7 @@ VMH_speed_DT_clean <- VMH_speed_DT[speed < Inf &
                                                                     weekdays(Transit_Day) %in% c("Monday","Tuesday","Wednesday","Thursday","Friday") ~ "Weekday",
                                                                     TRUE ~ weekdays(Transit_Day))
                                        ] 
+
 VMH_speed_DT_clean[
   ,.(.N
      ,mean=mean(speed)
@@ -57,90 +60,94 @@ VMH_speed_DT_clean[
      ,middle=quantile(speed, .50, na.rm=TRUE)
      ,upper=quantile(speed, .75, na.rm=TRUE)
      ,max=max(speed))
-  ,.(.id,direction)
+  ,.(lane_type,direction)
   ]
 
+VMH_speed_DT_clean[
+  ,.(median_speed = round(median(speed),1)
+    ,percentile_95 = round(quantile(speed,.95, na.rm = T),1))
+  ,.(lane_type,direction)]
 
-direction_medians <- VMH_speed_DT_clean[,median(speed),.(.id,direction)]
+direction_medians <- VMH_speed_DT_clean[,median(speed),.(lane_type,direction)]
 direction_medians
 
-peak_medians <- VMH_speed_DT_clean[,median(speed),.(.id,direction,Peak)]
+peak_medians <- VMH_speed_DT_clean[,median(speed),.(lane_type,direction,Peak)]
 peak_medians
 
-service_type_medians <- VMH_speed_DT_clean[,median(speed),.(.id,direction,Peak,Service_Type)]
+service_type_medians <- VMH_speed_DT_clean[,median(speed),.(lane_type,direction,Peak,Service_Type)]
 service_type_medians
 
 #plot by directind lane type
 VMH_speed_DT_clean %>% 
-  ggplot(aes(x = speed, color = .id,fill = .id)) +
+  ggplot(aes(x = speed, color = lane_type,fill = lane_type)) +
   geom_density(alpha = .3,size = .5) +
   facet_wrap(~direction) +
-  geom_vline(data = direction_medians,aes(xintercept = V1,color = .id))
+  geom_vline(data = direction_medians,aes(xintercept = V1,color = lane_type))
 
 VMH_speed_DT_clean %>%
   ggplot(aes(x = speed,color = direction, fill = direction))+
   geom_density(alpha = .3,size = .5) +
-  facet_wrap(~.id) +
+  facet_wrap(~lane_type) +
   geom_vline(data = direction_medians, aes(xintercept = V1, color = direction))
 
 #same but rel freq histograms
 VMH_speed_DT_clean[
   
-  ,.N,.(.id,direction,speed = floor(speed))][order(speed) 
-                                             ][,prop := N/sum(N),.(.id,direction)] %>%
-  ggplot(aes(x = speed, y = prop,color = .id, fill = .id)) +
+  ,.N,.(lane_type,direction,speed = floor(speed))][order(speed) 
+                                             ][,prop := N/sum(N),.(lane_type,direction)] %>%
+  ggplot(aes(x = speed, y = prop,color = lane_type, fill = lane_type)) +
   geom_bar(stat = "identity",alpha = .5,position = "dodge") +
   facet_wrap(~direction) +
-  geom_vline(data = direction_medians, aes(xintercept = V1, color = .id))
+  geom_vline(data = direction_medians, aes(xintercept = V1, color = lane_type))
 
 
 VMH_speed_DT_clean[
   
-  ,.N,.(.id,direction,speed = floor(speed))][order(speed) 
-                                             ][,prop := N/sum(N),.(.id,direction)] %>%
+  ,.N,.(lane_type,direction,speed = floor(speed))][order(speed) 
+                                             ][,prop := N/sum(N),.(lane_type,direction)] %>%
   ggplot(aes(x = speed, y = prop,color = direction, fill = direction)) +
   geom_bar(stat = "identity",alpha = .3,position = "identity") +
-  facet_wrap(~.id) + 
+  facet_wrap(~lane_type) + 
   geom_vline(data = direction_medians, aes(xintercept = V1, color = direction))
 
 
 #proportion by direction
-VMH_speed_DT_clean[  ,.N,.(.id,direction,speed = floor(speed))][order(speed) 
-][,prop := N/sum(N),.(.id,direction)] %>%
-  ggplot(aes(x = speed, y = prop,color = .id, fill = .id)) +
+VMH_speed_DT_clean[  ,.N,.(lane_type,direction,speed = floor(speed))][order(speed) 
+][,prop := N/sum(N),.(lane_type,direction)] %>%
+  ggplot(aes(x = speed, y = prop,color = lane_type, fill = lane_type)) +
   geom_bar(stat = "identity",alpha = .5,position = "identity") +
   facet_wrap(~direction)
 
-VMH_speed_DT_clean[ ,.N,.(.id,direction,speed = floor(speed))][order(speed) ][
-  ,prop := N/sum(N),.(.id,direction)] %>%
+VMH_speed_DT_clean[ ,.N,.(lane_type,direction,speed = floor(speed))][order(speed) ][
+  ,prop := N/sum(N),.(lane_type,direction)] %>%
   ggplot(aes(x = speed, y = prop,color = direction, fill = direction)) +
   geom_bar(stat = "identity",alpha = .5,position = "identity") +
-  facet_wrap(~.id)
+  facet_wrap(~lane_type)
 
 #peak by direction on one type of lane
-VMH_speed_DT_clean[.id == "single bi-directional"
-  ,.N,.(.id,direction,Peak,speed = floor(speed))
+VMH_speed_DT_clean[lane_type == "single bi-directional"
+  ,.N,.(lane_type,direction,Peak,speed = floor(speed))
 ][
-  ,prop := N/sum(N),.(.id,direction,Peak)
+  ,prop := N/sum(N),.(lane_type,direction,Peak)
 ] %>%
   ggplot(aes(x=speed, y = prop,color = Peak, fill = Peak)) + 
   geom_bar(alpha = .3,stat = "identity",position = "dodge")+
   facet_wrap(~direction) + theme_tufte()
 
-VMH_speed_DT_clean[.id == "separated uni-directional"
-                   ,.N,.(.id,direction,Peak,speed = floor(speed))
+VMH_speed_DT_clean[lane_type == "separated uni-directional"
+                   ,.N,.(lane_type,direction,Peak,speed = floor(speed))
                    ][
-                     ,prop := N/sum(N),.(.id,direction,Peak)
+                     ,prop := N/sum(N),.(lane_type,direction,Peak)
                      ] %>%
   ggplot(aes(x=speed, y = prop,color = Peak, fill = Peak)) + 
   geom_bar(alpha = .3,stat = "identity",position = "dodge")+
   facet_wrap(~direction) + theme_tufte()
 
 
-VMH_speed_DT_clean[.id == "none"
-                   ,.N,.(.id,direction,Peak,speed = floor(speed))
+VMH_speed_DT_clean[lane_type == "none"
+                   ,.N,.(lane_type,direction,Peak,speed = floor(speed))
                    ][
-                     ,prop := N/sum(N),.(.id,direction,Peak)
+                     ,prop := N/sum(N),.(lane_type,direction,Peak)
                      ] %>%
   ggplot(aes(x=speed, y = prop,color = Peak, fill = Peak)) + 
   geom_bar(alpha = .3,stat = "identity",position = "identity")+
@@ -150,26 +157,26 @@ VMH_speed_DT_clean[.id == "none"
 VMH_speed_DT_clean %>%
   ggplot(aes(x = speed,y = direction,color = direction, fill = direction)) +
   geom_boxplot(alpha = .5,outlier.alpha = .1) +
-  facet_wrap(~.id) + theme_solarized_2(light = T)
+  facet_wrap(~lane_type) + theme_solarized_2(light = T)
 
-VMH_speed_DT_clean[.id == "single bi-directional"] %>% 
+VMH_speed_DT_clean[lane_type == "single bi-directional"] %>% 
   ggplot(aes(x = speed, color = Peak,fill = Peak)) +
   geom_boxplot(alpha = .3) +
   facet_wrap(~direction)
 
-VMH_speed_DT_clean[.id == "none"] %>% 
+VMH_speed_DT_clean[lane_type == "none"] %>% 
   ggplot(aes(x = speed, color = Peak,fill = Peak)) +
   geom_boxplot(alpha = .3) +
   facet_wrap(~direction)
 
-VMH_speed_DT_clean[.id == "separated uni-directional"] %>% 
+VMH_speed_DT_clean[lane_type == "separated uni-directional"] %>% 
   ggplot(aes(x = speed, color = Peak,fill = Peak)) +
   geom_boxplot(alpha = .3) +
   facet_wrap(~direction)
 
 #investigate the hump
 VMH_speed_DT_clean[
-  .id == "none" & speed <= 15
+  lane_type == "none" & speed <= 15
 ][
   ,.N
   ,direction
